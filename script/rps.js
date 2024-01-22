be3c61c3b54ddc28f143e2ae5e92c765d1f5909c
Maheel Luqman <maheelllc@gmail.com>
2024-01-21 20:05:07 -0500

    Add game function and update playRound
    I created a game function that allows the player to play against the
    computer a best of 5 rounds game while disregarding ties as rounds. Once
    the computer and player play five rounds, the console prints out the
    final winner of the game. I had to update playRound to accomodate for
    the game function since I wanted to deal with the error handling of user
    input and forced replayability for tied rounds. Thus, I moved the
    case filtering to the game function and used tie's return value to stop
    the round counter from incrementing. I might rewrite this code again for
    more modularity and cleanliness.

--- script/rps.js
+++ script/rps.js
@@ -16,22 +16,88 @@ function getComputerChoice() {
     else {
         choice = 'Scissors';
     }
+    
     return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    let player = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    let player = playerSelection;
     let computer = computerSelection;
     if ((computer === 'Rock' && player === 'Scissors') || 
         (computer === 'Paper' && player === 'Rock') || 
         (computer === 'Scissors' && player === 'Paper')) {
-            return `You lose and you suck, you loser...${computer} beats ${player}`;
-        }
+            console.log(`You lose and you suck, you loser...${computer}` + 
+            ` beats ${player}`);
+            return -1;
+    }
     else if (computer == player) {
-        return `It\'s a tie. Do better next time. Both of you picked ${computer}.`;
+        console.log(`It\'s a tie. Do better next time. ` +
+        `Both of you picked ${computer}.`);
+        return 0;
     }
     else {
-        return 'You cheated. You hacked and beat my game and found out that ' + 
-        `the computer picked ${computer}.`;
+        console.log(`You cheated. You hacked and beat my game and found out ` +
+        `that the computer picked ${computer}, and ${player} beats ` +
+        `${computer}.`);
+        return 1;
     }
 }
+
+function game() {
+    // Let's store the scores of both players
+    let playerScore = 0;
+    let computerScore = 0;
+    // And let's store the result of each round
+    let round;
+    // Let's also store the number of non-tie rounds
+    let i = 0;
+    // For 5 rounds
+    while (i < 5) {
+        // Collect the player's choice
+        playerSelection = prompt("Rock, paper, or scissors? ");
+        playerSelection = playerSelection.charAt(0).toUpperCase() + 
+                          playerSelection.slice(1);
+        
+        // If the player provides an invalid input
+        while (playerSelection != 'Rock' && playerSelection != 'Paper' &&
+            playerSelection != 'Scissors') {
+            playerSelection = prompt("Please provide a valid option " +
+            "(rock, paper, or scissors): ");
+            playerSelection = playerSelection.charAt(0).toUpperCase() + 
+                              playerSelection.slice(1);
+        }
+        // Grab the computer's choice
+        computerSelection = getComputerChoice();
+        // Find out who won by playing the round
+        round = playRound(playerSelection, computerSelection);
+        // If round gives it to computer
+        if (round == -1) {
+            // Increment computer's score
+            computerScore++;
+        }
+        // else if round gives it to player
+        else if (round == 1) {
+            // Increment player's score
+            playerScore++;
+        }
+        // if it is a tie, we don't count it as a round
+        else {
+            continue;
+        }
+        i++;
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You won the 'best of 5' game. Good job...")
+    }
+    else if (computerScore > playerScore) {
+        console.log("You horribly lost to a random pic generator in a " +
+        "game of 5 rounds...")
+    }
+    else {
+        console.log("You tied with a random pick generator. Wow...");
+    }
+}
+
+
+game();
